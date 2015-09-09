141a5e4491448c15550ae3ad7481707fd511288e
Frank Liu <fzliu@yahoo-inc.com>
2015-09-09 02:38:17 -0700

    Fixed filename.

--- style.py
+++ style.py
@@ -1,5 +1,5 @@
 """
-transfer.py - An implementation of "A Neural Algorithm of Artistic Style"
+style.py - An implementation of "A Neural Algorithm of Artistic Style"
 by L. Gatys, A. Ecker, and M. Bethge. http://arxiv.org/abs/1508.06576.
 
 author: Frank Liu - frank@frankzliu.com
