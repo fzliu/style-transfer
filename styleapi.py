83d3a35b4f146e984052defe062c8c06b8ff4867
Frank Liu <fzliu@yahoo-inc.com>
2015-11-26 22:34:48 -0800

    Changed data transfer to using base64 JPG.

--- styleapi.py
+++ styleapi.py
@@ -19,8 +19,8 @@ from PIL import Image
 import redis
 from rq import Connection, Queue, SimpleWorker
 from rq import get_current_job
-from scipy.misc.pilutil import imshow
 from skimage.transform import rescale
+from skimage import img_as_ubyte
 
 # local imports
 from style import StyleTransfer
@@ -32,7 +32,6 @@ BASE_STYLE_ARGS = {"length": 512, "ratio": 1e5, "n_iter": 99}
 # argparse
 parser = argparse.ArgumentParser(description="Start up style processes, which read from Redis.",
                                  usage="styleapi.py -n <n_gpu> -r <redis_url>")
-parser.add_argument("-g", "--gpu-id", type=int, default=0, help="GPU to use (-1 for CPU)")
 parser.add_argument("-r", "--redis-url", type=str, required=True, help="redis DB URL")
 
 # style transfer objects, each backed by a lock, for use with style_api_init()
@@ -118,18 +117,25 @@ def style_api(img_style, img_content, callback=None):
 
     return img_out
 
-def deconstruct_image(img):
+def image_to_base64(img):
     """
-        Deconstructs an image (in numpy.ndarray form).
+        Converts a floating point numpy array to base64-encoded JPEG.
     """
 
-    img_data = {
-        "data": img.tostring(),
-        "shape": img.shape,
-        "dtype": img.dtype
-    }
+    # rescale image to max length of 512
+    scale = 512.0/max(img.shape[:2])
+    img = rescale(img, scale, order=3)
 
-    return img_data
+    # write the image to a buffer
+    pimg = Image.fromarray(img_as_ubyte(img))
+    buf = io.BytesIO()
+    pimg.save(buf, format="JPEG")
+    
+    # base64 encode
+    result = "data:image/jpg;base64,"
+    result += base64.b64encode(buf.getvalue())
+
+    return result
 
 def style_worker(pimg_style_data, pimg_content_data):
     """
@@ -151,16 +157,16 @@ def style_worker(pimg_style_data, pimg_content_data):
     # declare worker callback
     def callback(img_iter):
         job.meta["progress"] += 1
-        job.meta["result"] = deconstruct_image(img_iter)
+        job.meta["result"] = image_to_base64(img_iter)
         job.save()
 
     # perform style transfer
     args = BASE_STYLE_ARGS.copy()
     args.update({"init": "content", "callback": callback})
     st_inst.transfer_style(img_style, img_content, **args)
-
     img_result = st_inst.get_generated()
-    result = deconstruct_image(img_result)
+    result = image_to_base64(img_result)
+
     return result
 
 def main(args):
@@ -169,9 +175,10 @@ def main(args):
 
         Executing this script will cause the process to listen for jobs from a Redis DB.
         This assumes that you have the right libraries (redis and rq) already installed.
-        For small-scale demos, import this module and call style_api() instead.
+        For small-scale demos, import this module and directly call style_api() instead.
     """
 
+    # start it up
     conn = redis.from_url(args.redis_url)
     with Connection(conn):
         worker = SimpleWorker(Queue("default"))
